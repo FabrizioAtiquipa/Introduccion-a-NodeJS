32f5035be1831fb30c0ef4849b06da94fe28a877
FabrizioAtiquipa <fabrizio.atiquipa@tecsup.edu.pe>
2021-04-24 22:10:03 -0500

    4. delete one

--- introduccion-node/index.js
+++ introduccion-node/index.js
@@ -1,53 +1,60 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 let persons = [
-   {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456"
-    },
-   {
-      id: 2,
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
-    },
-   {
-      id: 3,
-      name: "Dan Abramov",
-      number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    },
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/info', (request, response) => {
-    const Fecha=new Date()
-    const vista=`<h5>Phonebook has info for ${persons.length} people</h5>
-                 <p>${Fecha}</p>`
+app.get("/info", (request, response) => {
+  const Fecha = new Date();
+  const vista = `<h5>Phonebook has info for ${persons.length} people</h5>
+                 <p>${Fecha}</p>`;
 
-    response.send(vista)
-})
+  response.send(vista);
+});
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-    const ID = request.params.id
-    const PersonaID = persons.find(person => person.id == ID)
+app.get("/api/persons/:id", (request, response) => {
+  const ID = request.params.id;
+  const PersonaID = persons.find((person) => person.id == ID);
 
-    if (PersonaID) {
-        response.json(PersonaID)
-    } else {
-        response.status(404).end()
-    }
-})
+  if (PersonaID) {
+    response.json(PersonaID);
+  } else {
+    response.status(404).end();
+  }
+});
 
-const PORT = 3001
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((persons) => persons.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`El servidor esta corriendo en el puerto:${PORT}`)
-})
+  console.log(`El servidor esta corriendo en el puerto:${PORT}`);
+});
